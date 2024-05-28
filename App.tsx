efa082aacec718e1454151f7a866abec4bd9fe81
Morgen Diaz <morgen@webslinger.works>
2024-05-28 11:46:25 -1000

    Create major sections search and list.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchContainer}>
+          <Text>Search Area</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>List area</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
@@ -15,7 +23,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  searchContainer: {
+    backgroundColor: "green",
+    padding: 20,
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 20,
   },
 });
