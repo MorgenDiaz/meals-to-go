f5e70f89d102df160ae59f0827b9866df53867d3
Morgen Diaz <morgen@webslinger.works>
2024-06-02 11:29:00 -1000

    Create theme configuration.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import RestaurantsScreen from "./src/features/restaurants/screens/RestaurantsScreen";
+import RestaurantsScreen from "./src/presentation/features/restaurants/screens/RestaurantsScreen";
+import {
+  Oswald_400Regular,
+  useFonts as useOswald,
+} from "@expo-google-fonts/oswald";
+import { Lato_400Regular, useFonts as useLato } from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [isOswaldLoaded] = useOswald({ Oswald_400Regular });
+  const [isLatoLoaded] = useLato({ Lato_400Regular });
+
+  if (!isOswaldLoaded || !isLatoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <RestaurantsScreen />
